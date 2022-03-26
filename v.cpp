8a0f098e77b331f2fd9608fe45cb53d57e5293a0
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-03-26 22:11:38 +0700

    sua xau v dua len github

--- v.cpp
+++ v.cpp
@@ -9,9 +9,10 @@ int main() {
     int count = 0;
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == c) {
-            count++;
+        	cout << i << endl;
+            count++; 
         }
     }
-    cout << count;
+    cout << count << endl;
     return 0;
 }
