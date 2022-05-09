12235345a78e95876df613ccf81bb48bde8f5646
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-05-09 23:59:40 +0700

    nhap_vao_so_nguyen_n_và_hien_thi_ra_nguyen_am_nguyen_duong_hoac_bang_0

---
+++ nhap_vao_so_nguyen_n_và_hien_thi_ra_nguyen_am_nguyen_duong_hoac_bang_0.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n > 0) {
+		cout << "n la so duong" << endl;
+	}
+	if (n < 0) {
+		cout << "n is a negative number" << endl;
+	} 
+	if (n == 0) {
+		cout << "n is equal to 0" << endl;
+	}
+	return 0;
+}
