231a02124ec1997564fad865fd0288217a439101
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-05-12 20:34:32 +0700

    nam_nay_toi_21_tuoi_toi_co_nhieu_hoai_bao_muon_theo_duoi

---
+++ nam_nay_toi_21_tuoi_toi_co_nhieu_hoai_bao_muon_theo_duoi.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main(int argc, char *argv[]) 
+{ 
+ int age; 
+ age=21; 
+ cout<<" Toi nam nay "<<age<<" tuoi" << endl; 
+ cout<<" Toi co nhieu hoai bao muon theo duoi" << endl; 
+ 
+ return 0; 
+}
+
