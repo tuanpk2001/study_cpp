b6ca0519390c710d24249a49aba277854be61540
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-05-20 23:33:14 +0700

    hien_thi_gia_tri_a_la_duong_hay_am

---
+++ hien_thi_gia_tri_a_la_duong_hay_am.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	if (a > 0) {
+		cout << "a la so duong";
+	}
+	else if (a < 0) {
+		cout << "a la so am";
+	}
+	else {
+		cout << "a bang khong";
+	}
+
+	return 0;
+}
