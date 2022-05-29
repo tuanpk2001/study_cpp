1fecfa0d4a9ec7f90dfcb01d048cb01e20cdaa48
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-05-29 22:23:39 +0700

    tinh_tong_cua_10_so_nguyen

---
+++ tinh_tong_cua_10_so_nguyen.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        cin >> a;
+        sum += a;
+    }
+    cout << sum;
+    return 0;
+}
