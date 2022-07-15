bbfc787d6e025c056d755d1a3b6af9f1d0be60c2
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-07-15 13:52:59 +0700

    bai_tap_3_chuong_2_codelearn

---
+++ bai_3_II.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std ;
+
+int main(){
+	int a = 6;
+	int b = 2;
+	cout << "a + b = " << a + b << endl;
+	cout << "a - b = " << a - b << endl;
+	cout << "a * b = " << a * b << endl;
+	cout << "a / b = " << a / b << endl;
+	return 0;
+}
