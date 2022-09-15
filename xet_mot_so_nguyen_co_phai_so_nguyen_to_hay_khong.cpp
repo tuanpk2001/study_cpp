ae1b4a6aac421721f29c6bce94cbb8ab8970293a
Pham Khac Tuan <tuanpkz230901@gmail.com>
2022-09-15 15:08:28 +0700

    xet_mot_so_nguyen_co_phai_so_nguyen_to_hay_khong

---
+++ xet_mot_so_nguyen_co_phai_so_nguyen_to_hay_khong.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int kiemtra(int n){
+	if(n < 2){
+		cout << "False";
+	}
+	int dem = 0;
+	for(int i = 2; i <= sqrt(n); i++){
+		if( n % i == 0){
+			dem ++;
+			break;
+		}
+	}
+	if(dem == 0){
+		cout << n << " True" << endl;
+	}else{
+		cout << n << " False" << endl;
+	}
+	return n;
+}
+
+int main(){
+	int n;
+	cout << "nhap n: ";
+	cin >> n;
+	kiemtra(n);
+}
